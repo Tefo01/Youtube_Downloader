2224256789bb3a994adb08bc4bdd193a461f2b05
Tefo01 <ttarici0@gmail.com>
2022-12-30 13:17:49 +0300

    fixed the bug

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from pytube import YouTube
 
-def Download(link):
-    youtube=YouTube(link)
-    youtube=youtube.streams.get_highest_resolution()
+link = input("put your youtube link here URL: ")
+
+
+def download(asd):
+    print("1")
+    youtube = YouTube(asd)
+    youtube = youtube.streams.get_highest_resolution()
+
     try:
         youtube.download()
     except:
         print("there has been an error")
-    print("this download is completed")
 
-    link=input("put your youtube link here URL: ")
-    Download(link)
+    print("this download is completed")
+    print("2")
+
+
+download(link)
